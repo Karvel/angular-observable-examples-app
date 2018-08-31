c798e6877e68714feba7d35138c5f973ff235c45
Elanna Grossman <elanna.grossman@gmail.com>
2018-08-30 19:41:45 -0700

    feat(app): import `HttpClientModule` and `ReactiveFormsModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule }       from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule }    from '@angular/common/http';
 import { NgModule }            from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent }        from './app.component';
 import {
@@ -15,6 +16,8 @@ import {
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
     AppRoutingModule,
   ],
   providers: [],
