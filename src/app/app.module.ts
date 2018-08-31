a0728dbd6df29f9fe12967abf9a4ea0bce2851b7
Elanna Grossman <elanna.grossman@gmail.com>
2018-08-30 19:41:17 -0700

    feat(app): wire up `AppRoutingModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule }       from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgModule }            from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent }        from './app.component';
+import {
+  AppRoutingModule,
+  routedComponents as mainAppRoutedComponents,
+}                              from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    mainAppRoutedComponents,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
