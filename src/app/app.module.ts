9f218a1b3d4fa3c807630e56b099416f83ba4362
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-02 23:18:51 -0700

    feat(app): wire up `NavigationComponent`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,13 @@ import {
   routedComponents as mainAppRoutedComponents,
 }                              from './app-routing.module';
 import { CoreModule }          from './core/core.module';
+import { NavigationComponent } from './features/navigation/navigation.component';
 import { PipeModule }          from './core/pipes/pipe.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
     mainAppRoutedComponents,
   ],
   imports: [
