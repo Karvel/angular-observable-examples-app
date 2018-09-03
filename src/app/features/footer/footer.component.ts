5d5710afdc04dea45c988e174ef815958b94620f
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-03 13:03:10 -0700

    feat(footer): add `FooterComponent`

---
+++ src/app/features/footer/footer.component.ts
@@ -0,0 +1,16 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+
+@Component({
+  selector        : 'app-footer',
+  templateUrl     : './footer.component.html',
+  styleUrls       : ['./footer.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush,
+})
+export class FooterComponent {
+  public dateYear: number = new Date().getFullYear();
+
+  constructor() {  }
+}
