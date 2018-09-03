786cee066c8befb8cd39056ed3e25827471eaa6d
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-02 23:18:37 -0700

    feat(navigation): add `NavigationComponent`

---
+++ src/app/features/navigation/navigation.component.ts
@@ -0,0 +1,23 @@
+import { Component }  from '@angular/core';
+
+import { RoutedLink } from '../../core/models/routed-link';
+
+@Component({
+  selector    : 'app-navigation',
+  templateUrl : './navigation.component.html',
+  styleUrls   : ['./navigation.component.scss'],
+})
+export class NavigationComponent {
+  public linkList: RoutedLink[] = [
+    {
+      name  : 'Home',
+      route : '',
+    },
+    {
+      name  : 'Contact',
+      route : '/contact',
+    },
+  ];
+
+  constructor() { }
+}
