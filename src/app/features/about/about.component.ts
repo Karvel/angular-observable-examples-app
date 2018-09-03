1945bedd857f1e9acd20e0c3ae0b352861838488
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-03 12:39:30 -0700

    feat(about): add `AboutComponent`

---
+++ src/app/features/about/about.component.ts
@@ -0,0 +1,14 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector        : 'app-about',
+  templateUrl     : './about.component.html',
+  styleUrls       : ['./about.component.scss'],
+  changeDetection : ChangeDetectionStrategy.OnPush,
+})
+export class AboutComponent {
+  constructor() { }
+}
