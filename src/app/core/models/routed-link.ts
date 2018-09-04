5d4ce02d56760c5a56931a1ec75f7914f29524eb
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-03 23:01:07 -0700

    feat(footer): add footer links

--- src/app/core/models/routed-link.ts
+++ src/app/core/models/routed-link.ts
@@ -2,3 +2,10 @@ export interface RoutedLink {
   name: string;
   route: string;
 }
+
+export interface ExternalLink {
+  name: string;
+  linkPath: string;
+  icon: string;
+}
+
--- src/app/features/footer/footer.component.ts
+++ src/app/features/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import {
   Component,
   ChangeDetectionStrategy,
-} from '@angular/core';
+}                       from '@angular/core';
+
+import { ExternalLink } from '../../core/models/routed-link';
 
 @Component({
   selector        : 'app-footer',
@@ -10,7 +12,38 @@ import {
   changeDetection : ChangeDetectionStrategy.OnPush,
 })
 export class FooterComponent {
+  public contactList: ExternalLink[] = [
+    {
+      linkPath : 'http://hapax-legomenon.net/',
+      name     : 'Hapax Legomenon',
+      icon     : 'fa fa-link',
+    },
+    {
+      linkPath : 'https://github.com/Karvel',
+      name     : 'GitHub',
+      icon     : 'fa fa-github-square',
+    },
+    {
+      linkPath : 'https://medium.com/@elanna.grossman',
+      name     : 'Medium',
+      icon     : 'fa fa-medium',
+    },
+    {
+      linkPath : 'https://www.linkedin.com/in/elannagrossman',
+      name     : 'LinkedIn',
+      icon     : 'fa fa-linkedin',
+    },
+    {
+      linkPath : 'https://www.flickr.com/photos/jadeilyn/',
+      name     : 'Flickr',
+      icon     : 'fa fa-flickr',
+    },
+  ];
   public dateYear: number = new Date().getFullYear();
 
-  constructor() {  }
+  constructor() { }
+
+  public trackByName(index: number, item: ExternalLink): string {
+    return item.name;
+  }
 }
