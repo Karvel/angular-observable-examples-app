b44723531c554d5e009058fc695d12e358c33c85
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-04 20:27:15 -0700

    feat(app): add and call `setPageTitle()`, import references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,55 @@ import {
   OnDestroy,
   OnInit,
 }                       from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+}                       from '@angular/router';
+import { Title }        from '@angular/platform-browser';
 
+import {
+  filter,
+  map,
+  mergeMap,
+}                       from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 @Component({
   selector    : 'app-root',
   templateUrl : './app.component.html',
   styleUrls   : ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
-  title = 'angular-base-app';
+  public title: string = 'angular-observable-examples-app';
+
+  private routerEventsSubscription: Subscription;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+  ) { }
+
   ngOnInit(): void {
+    this.setPageTitle();
   }
+
   ngOnDestroy(): void {
+    this.routerEventsSubscription.unsubscribe();
+  }
+
+  private setPageTitle(): void {
+    this.routerEventsSubscription = this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => this.activatedRoute),
+        map(route => {
+          while (route.firstChild) { route = route.firstChild; }
+          return route;
+        }),
+        filter(route => route.outlet === 'primary'),
+        mergeMap(route => route.data),
+      )
+      .subscribe(event => this.titleService.setTitle(`${event['title']} - Angular Observable Examples`));
   }
 }
