cccf8bab42c62d021a176b49fe41049b8e14805c
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-04 20:25:20 -0700

    feat(app): add lifecycle hooks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnDestroy,
+  OnInit,
+}                       from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'angular-base-app';
+  ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+  }
 }
