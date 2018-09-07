4ed8106951229791f1adbe6969fdc2c8f22dcde4
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-06 20:36:15 -0700

    feat(phone-validation): improve validation message

--- src/app/core/validation/phone-validation.ts
+++ src/app/core/validation/phone-validation.ts
@@ -10,7 +10,7 @@ export class PhoneValidation {
   static validPhone(isRequired?: boolean): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
       if (!control.value) {
-        return isRequired ? { 'invalidPhone' : `Phone num is required.` } : null;
+        return isRequired ? { 'invalidPhone' : `Phone number is required.` } : null;
       }
       const strippedValue = control.value.replace(CONSTANTS.NON_DECIMAL_DIGITS, '');
       if (!CONSTANTS.PHONE_REGEX.test(strippedValue)) {
