0e03fe7eb43bff28fc9b164c123883718ead8b17
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-06 20:41:43 -0700

    feat(contact): wire up initial contact form

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -2,6 +2,10 @@ import {
   Component,
   ChangeDetectionStrategy,
 } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector        : 'app-contact',
@@ -10,5 +14,14 @@ import {
   changeDetection : ChangeDetectionStrategy.OnPush,
 })
 export class ContactComponent {
-  constructor() { }
+  public contactForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.contactForm = this.formBuilder.group({
+      email: '',
+      phone: '',
+    });
+  }
 }
