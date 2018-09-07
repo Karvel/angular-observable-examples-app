5c73474d2f086344a36cba03985a0d9ca4ca46c3
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-06 20:28:35 -0700

    feat(validation): add `PhoneValidation`

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,7 +1,11 @@
 interface Constants {
   readonly EMAIL_REGEX: RegExp;
+  readonly PHONE_REGEX: RegExp;
+  readonly NON_DECIMAL_DIGITS: RegExp;
 }
 
 export const CONSTANTS: Constants = {
-  EMAIL_REGEX : /^(?!\.)(?!.*?\.(\.|@))[\w\d.!#$%&'*+\-\/=?^_`{|}~]+@[\w\d.-]+\.[\w\d]{2,}$/,
+  EMAIL_REGEX        : /^(?!\.)(?!.*?\.(\.|@))[\w\d.!#$%&'*+\-\/=?^_`{|}~]+@[\w\d.-]+\.[\w\d]{2,}$/,
+  PHONE_REGEX        : /(^\d{10}$)/,
+  NON_DECIMAL_DIGITS : /\D/g,
 };
