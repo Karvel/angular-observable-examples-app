e063ef4eb472b20092e407ae47c38d2d3d2403e3
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-23 12:15:13 -0700

    feat(constants): add RegEx patterns

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,11 +1,15 @@
 interface Constants {
+  readonly DECIMAL_DIGITS_ONLY: RegExp;
   readonly EMAIL_REGEX: RegExp;
-  readonly PHONE_REGEX: RegExp;
   readonly NON_DECIMAL_DIGITS: RegExp;
+  readonly PHONE_REGEX: RegExp;
+  readonly SYMBOL_REGEX: RegExp;
 }
 
 export const CONSTANTS: Constants = {
-  EMAIL_REGEX        : /^(?!\.)(?!.*?\.(\.|@))[\w\d.!#$%&'*+\-\/=?^_`{|}~]+@[\w\d.-]+\.[\w\d]{2,}$/,
-  PHONE_REGEX        : /(^\d{10}$)/,
-  NON_DECIMAL_DIGITS : /\D/g,
+  DECIMAL_DIGITS_ONLY : /[^0-9.]/g,
+  EMAIL_REGEX         : /^(?!\.)(?!.*?\.(\.|@))[\w\d.!#$%&'*+\-\/=?^_`{|}~]+@[\w\d.-]+\.[\w\d]{2,}$/,
+  NON_DECIMAL_DIGITS  : /\D/g,
+  PHONE_REGEX         : /(^\d{10}$)/,
+  SYMBOL_REGEX        : /[^-+_!@#$%^&*,.?]/g,
 };
