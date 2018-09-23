b8f713b9a75df34b91e38e09a5aa995136a66dcc
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-23 12:58:44 -0700

    feat(constants): update RegEx patterns

--- src/app/core/utils/constants.ts
+++ src/app/core/utils/constants.ts
@@ -1,5 +1,6 @@
 interface Constants {
   readonly DECIMAL_DIGITS_ONLY: RegExp;
+  readonly DIGIT_REGEX: RegExp;
   readonly EMAIL_REGEX: RegExp;
   readonly NON_DECIMAL_DIGITS: RegExp;
   readonly PHONE_REGEX: RegExp;
@@ -8,8 +9,9 @@ interface Constants {
 
 export const CONSTANTS: Constants = {
   DECIMAL_DIGITS_ONLY : /[^0-9.]/g,
+  DIGIT_REGEX         : /[0-9]/g,
   EMAIL_REGEX         : /^(?!\.)(?!.*?\.(\.|@))[\w\d.!#$%&'*+\-\/=?^_`{|}~]+@[\w\d.-]+\.[\w\d]{2,}$/,
   NON_DECIMAL_DIGITS  : /\D/g,
   PHONE_REGEX         : /(^\d{10}$)/,
-  SYMBOL_REGEX        : /[^-+_!@#$%^&*,.?]/g,
+  SYMBOL_REGEX        : /[-+_!@#$%^&*,.?]/g,
 };
