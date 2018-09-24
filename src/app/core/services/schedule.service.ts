79e026b4cb59dac91fbbdc0c234a844051d0b189
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-23 20:09:04 -0700

    feat(core): add and wire up `ScheduleService`

---
+++ src/app/core/services/schedule.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { map }        from 'rxjs/operators';
+
+import { ApiService } from '../api/api.service';
+
+@Injectable()
+export class ScheduleService {
+  constructor(
+    private apiService: ApiService,
+  ) { }
+
+  public getSchedule(): Observable<any> {
+    return this.apiService.schedule.getSchedule().pipe(map((response) => response));
+  }
+
+  public addScheduleItem(event): any {
+    return this.apiService.schedule.addScheduleItem(event)
+      .then((response) => response);
+  }
+}
