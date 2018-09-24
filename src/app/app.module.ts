56d467238de571c5df1d7f2080383f040c5ad713
Elanna Grossman <elanna.grossman@gmail.com>
2018-09-23 18:53:40 -0700

    feat(app): wire up `AngularFireModule`, `AngularFireDatabaseModule`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule }           from '@angular/platform-browser';
-import { HttpClientModule }        from '@angular/common/http';
-import { MaterialModule }          from './core/imports/material.module';
-import { NgModule }                from '@angular/core';
-import { ReactiveFormsModule }     from '@angular/forms';
+import { BrowserAnimationsModule }   from '@angular/platform-browser/animations';
+import { BrowserModule }             from '@angular/platform-browser';
+import { HttpClientModule }          from '@angular/common/http';
+import { MaterialModule }            from './core/imports/material.module';
+import { NgModule }                  from '@angular/core';
+import { ReactiveFormsModule }       from '@angular/forms';
 
-import { AppComponent }            from './app.component';
+import { AngularFireModule }         from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+import { AppComponent }              from './app.component';
 import {
   AppRoutingModule,
   routedComponents as mainAppRoutedComponents,
-}                                  from './app-routing.module';
-import { ControlsModule }          from './core//controls/controls.module';
-import { CoreModule }              from './core/core.module';
-import { FooterComponent }         from './features/footer/footer.component';
-import { NavigationComponent }     from './features/navigation/navigation.component';
-import { PipeModule }              from './core/pipes/pipe.module';
+}                                    from './app-routing.module';
+import { ControlsModule }            from './core//controls/controls.module';
+import { CoreModule }                from './core/core.module';
+import { environment }               from '../environments/environment';
+import { FooterComponent }           from './features/footer/footer.component';
+import { NavigationComponent }       from './features/navigation/navigation.component';
+import { PipeModule }                from './core/pipes/pipe.module';
 
 @NgModule({
   declarations: [
@@ -28,6 +32,8 @@ import { PipeModule }              from './core/pipes/pipe.module';
     BrowserAnimationsModule,
     ControlsModule,
     CoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
     HttpClientModule,
     MaterialModule,
     PipeModule.forRoot(),
