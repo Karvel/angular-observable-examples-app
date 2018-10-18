ff4c19d3b0617f8fee9f23de574554f855eb9f77
Elanna Grossman <elanna.grossman@gmail.com>
2018-10-17 21:26:43 -0700

    feat(app): remove `AboutComponent`, `HomeComponent`, references

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,21 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutComponent }       from './features/about/about.component';
-import { HomeComponent }        from './features/home/home.component';
-import { ResourcesComponent }   from './features/resources/resources.component';
 import { NotFoundComponent }    from './features/not-found/not-found.component';
+import { ResourcesComponent }   from './features/resources/resources.component';
 
 const routes: Routes = [
- {
-   path      : '',
-   component : HomeComponent,
-   data      : { title: 'Home' },
- },
- {
-   path      : 'about',
-   component : AboutComponent,
-   data      : { title: 'About' },
- },
- {
+  {
+    path       : '',
+    redirectTo : 'examples',
+    pathMatch  : 'full',
+  },
+  {
    path      : 'resources',
    component : ResourcesComponent,
    data      : { title: 'Resources' },
- },
- {
+  },
+  {
    path      : '**',
    component : NotFoundComponent,
    data      : { title: 'Not Found' },
@@ -36,8 +29,6 @@ const routes: Routes = [
 export class AppRoutingModule {}
 
 export const routedComponents = [
-  AboutComponent,
-  HomeComponent,
   NotFoundComponent,
   ResourcesComponent,
 ];
