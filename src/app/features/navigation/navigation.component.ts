ab3360cb3de2933ca8a9ff3afc48e56a085fe3bb
Elanna Grossman <elanna.grossman@gmail.com>
2018-10-19 00:49:46 -0700

    feat(resources): add link

--- src/app/features/navigation/navigation.component.ts
+++ src/app/features/navigation/navigation.component.ts
@@ -22,7 +22,7 @@ export class NavigationComponent {
       route : '/resources',
     },
     {
-      name  : 'Populate Databases',
+      name  : 'Seed Databases',
       route : '/start',
     },
   ];
