cacab4ffc91dcda88ab50c5e30c42cbab62bc059
Elanna Grossman <elanna.grossman@gmail.com>
2018-10-19 00:43:19 -0700

    refactor(higher-order-2): simplify logic, remove dead code

--- src/app/core/models/company.ts
+++ src/app/core/models/company.ts
@@ -32,9 +32,3 @@ export const mockCompanyList: ICompany[] = [
     color: 'black',
   },
 ];
-
-/*
-  This is an empty array in this commit for the purposes of resetting the company for the tutorial.
-  It references keys specific to my existing firebase collection and so was committed as a stub.
-*/
-export const pristineCompanyList: ICompany[] = [];
