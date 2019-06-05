3244afc0ae7fc4f8d02b8726c62eee292d9f32a1
Elanna Grossman <elanna.grossman@gmail.com>
2019-06-05 13:42:59 -0700

    feat(core): add and wire up `MaskDirective`

--- src/app/core/pipes/pipe.module.ts
+++ src/app/core/pipes/pipe.module.ts
@@ -1,16 +1,19 @@
 import { NgModule }           from '@angular/core';
 
 import { FirstFormErrorPipe } from './first-form-error.pipe';
+import { MaskDirective }      from '../directives/mask.directive';
 import { SafeHTMLPipe }       from './safe-html.pipe';
 
 
 @NgModule({
   declarations: [
     FirstFormErrorPipe,
+    MaskDirective,
     SafeHTMLPipe,
   ],
   exports: [
     FirstFormErrorPipe,
+    MaskDirective,
     SafeHTMLPipe,
   ],
 })
